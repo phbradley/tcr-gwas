e93549c15a168e8ff1f281b1095ec5442355c14c
magdalenarussell <magdalenarussell23@gmail.com>
2020-07-16 15:27:25 -0700

    changing correlate snps protocol and plotting functions

--- trimming_regression/correlate_snps.R
+++ trimming_regression/correlate_snps.R
@@ -8,189 +8,63 @@ library("SNPRelate")
 library("GWASTools")
 
 
-# Need to first define a cutoff for correlating snps with one another. 
-# Let's define a proximity cutoff and a p_value difference cutoff
-
-bonferroni = 0.05/35481497
-proximity_cutoff = 1000
-pvalue_cutoff = 3.594536e-05 #0.05/nrow(together)
-
-# This method finds a bonferonni significant snp, then searches forward and backward in space until the snps are either 1. too far away (with proximity_cutoff) or 2. not significant enough (with pvalue_cutoff)
-
-correlate_snps <- function(snp_id_list, snp_meta_data, productivity, trim_type, bonferroni, proximity_cutoff, pvalue_cutoff){
-    if (nrow(snp_meta_data) == 11267){
-        snp_meta_data = snp_meta_data[,mean(mwu_pval), by = .(snpnum, chromosome, hg19_pos)]
-        snp_meta_data$snpnum = paste0("snp", snp_meta_data$snpnum)
-        colnames(snp_meta_data) = c("snp", "chr", "hg19_pos", "phile_pval")
-    } else if (colnames(snp_meta_data)[1] == "chromosome"){
-        snp_meta_data$snpid = paste0("snp", snp_meta_data$snpid)
-        colnames(snp_meta_data) = c("chr", "feature", "hg19_pos", "phil_pval", "snp")
-    } else {
-        snp_meta_data = snp_meta_data[,1:3]
-        snp_meta_data$snpid = paste0("snp", snp_meta_data$snpid)
-        colnames(snp_meta_data) = c("snp", "hg19_pos", "chr")
-    }
-
-    regression_snp_list = as.data.table(read.table(paste0("regression_bootstrap_results/", productivity, "/", trim_type, "/", trim_type, "_", productivity, "_snplist_", snp_id_list[1], "-",snp_id_list[length(snp_id_list)], "_snps_varying_intercepts_by_subject.tsv"), header = TRUE))
-    together = merge(regression_snp_list, snp_meta_data, all.x = TRUE, by = "snp")
-
-    correlated_snps = data.table()
-    j = 1
-    together = together[order(hg19_pos)]
-
-    while (j < nrow(together)){
-        if (together[j]$pvalue < bonferroni){
-            sig_snp_temp = data.table()
-            index = j
-            for (i in seq(j-1, 1)){
-                if (index == 1){ break }
-                else if ((abs(together[i]$hg19_pos - together[index]$hg19_pos )<= proximity_cutoff) && (together[i]$pvalue < pvalue_cutoff) && (together[i]$chr == together[index]$chr)){
-                    sig_snp_temp = rbind(sig_snp_temp, together[i])
-                    index = index - 1 
-                } else { break }  
-            }
-
-            sig_snp_temp = rbind(sig_snp_temp, together[j])
-
-            index = j
-            for(k in seq(j+1, nrow(together))){
-                if ((abs(together[k]$hg19_pos - together[index]$hg19_pos)<= proximity_cutoff) && (together[k]$pvalue < pvalue_cutoff)){
-                    sig_snp_temp = rbind(sig_snp_temp, together[k]) 
-                    index = index + 1 
-                } else { break }
-            }
-
-            correlated_snps = rbind(correlated_snps, data.table(index = j, sig_snp_temp))  
-            j = j + nrow(sig_snp_temp)
-        } else {
-            j = j + 1
-        }
-    }
-    print("finished correlating snps")
-    if (nrow(correlated_snps) >= 1){
-        if (ncol(correlated_snps) == 10){
-            colnames(correlated_snps) = c("index","snp", "intercept", "slope", "standard_error", "p", "chr", "feature", "start", "phil_pval")
-        }
-    }
-    return(correlated_snps)
-}
-
-
-group_p_values <- function(snp_id_list, snp_meta_data, productivity, trim_type, bonferroni, proximity_cutoff, pvalue_cutoff){
-    correlated_snps = correlate_snps(snp_id_list, snp_meta_data, productivity, trim_type, bonferroni, proximity_cutoff, pvalue_cutoff)
-    if (nrow(correlated_snps) >= 1){
-        correlated_snps_with_group_p_val = data.table()
-        snps_gds = snpgdsOpen("../_ignore/snp_data/HSCT_comb_geno_combined_v03_tcr.gds")
-        n <- index.gdsn(snps_gds, "sample.id")
-        sampleid <- read.gdsn(n)
-        for (i in unique(correlated_snps$index)){
-            if(length(correlated_snps$feature) > 0){
-                snps = unique(correlated_snps[index == i][,-c(8,10)])
-            } else {
-                snps = unique(correlated_snps[index == i])
-            }
-            if (nrow(snps)>1){
-                genotype_list = data.table(scanID = c(sampleid))
-                colnames(genotype_list) = c("scanID")
-                genotype_list$scanID = as.numeric(as.character(genotype_list$scanID))
-                subject_id_mapping = as.data.table(read.table('../_ignore/snp_data/gwas_id_mapping.tsv', sep = "\t", fill=TRUE, header = TRUE, check.names = FALSE))
-                genotypes_subjects = merge(genotype_list, subject_id_mapping, by = "scanID")
-                for (snp in snps$snp){
-                    snp_id = as.numeric(substring(snp, 4))
-                    genotype = snpgdsGetGeno(snps_gds, snp.id=snp_id)
-                    genotypes_df = data.table(genotype)
-                    colnames(genotypes_df) = c(snp)
-                    # Convert subject names and compile condensed data: 
-                    genotypes_subjects = cbind(genotypes_subjects, genotypes_df)
-                }
-                #This may throw an error if there are NA entries: 
-                snps$group_p_val = COMBAT(x = snps$p, snp.ref = genotypes_subjects[,-c(1,2)])[1]
-            } else {
-                snps$group_p_val = snps$p
-            }
-            correlated_snps_with_group_p_val= rbind(correlated_snps_with_group_p_val, snps)
-        }
-        closefn.gds(snps_gds)
-        print("finished p-value grouping")
-
-        name = paste0('regression_bootstrap_results/', productivity, '/', trim_type, '/', trim_type, '_correlated_snps_from_snplist_', snp_id_list[1], "-",snp_id_list[length   (snp_id_list)], '_snps_varying_intercepts_by_subject.tsv')
-
-        write.table(correlated_snps_with_group_p_val, file= name, quote=FALSE, sep='\t', col.names = NA)
-    } else {
-        print("no correlated snps!!!")
-    } 
-}
-
-#condense_correlated_snps_pvals <- function(correlated_snps){
-
-#}
-
-correlated_snps_LD <- function(snp_list){
+correlate_snps_ld <- function(chrom, snp_list, cutoff){
     snps_gds = snpgdsOpen("../_ignore/snp_data/HSCT_comb_geno_combined_v03_tcr.gds")
-    sampleid <- read.gdsn(index.gdsn(snps_gds, "sample.id"))
     snp_chrom <- read.gdsn(index.gdsn(snps_gds, "snp.chromosome"))
-    all_genotypes = data.table(localID = sampleid)
-    index_start = 1
-    for (chrom in c(1:22, 'X')){
-        snps_count_chr = length(snp_chrom[snp_chrom == chrom])
-        index_end = index_start + snps_count_chr - 1
+    sampleid <- read.gdsn(index.gdsn(snps_gds, "sample.id"))
 
-        snp_id <- read.gdsn(index.gdsn(snps_gds, "snp.id"), start = index_start, count=(index_end-index_start))
-        
-        genotypes <- read.gdsn(index.gdsn(snps_gds, "genotype"), start=c(1,index_start), count = c(398, (index_end-index_start)))
-        index_start = index_end + 1
+    chr_index_start = match(chrom,snp_chrom)
+    chr_index_end = match(chrom+1,snp_chrom)-1
 
-        genotypes_matrix = as.matrix(genotypes)
-        genotypes_matrix[genotypes_matrix==3]<-NA
-        colnames(genotypes_matrix) = snp_id
-        genotypes_matrix_noNA = Filter(function(x) length(unique(x))!=1, snps)
-        snps_no_NA = Filter(function(x) length(unique(snps_no_NA[x != "NA"])) != 1, snps_no_NA)
-        ld.Rsquare(genotypes_matrix)
+    snp_pos <- read.gdsn(index.gdsn(snps_gds, "snp.position"), start = chr_index_start, count=(chr_index_end-chr_index_start)+1)
+    snp_id <- read.gdsn(index.gdsn(snps_gds, "snp.id"), start = chr_index_start, count=(chr_index_end-chr_index_start)+1)
+    genotypes <- read.gdsn(index.gdsn(snps_gds, "genotype"), start = c(1, chr_index_start), count=c(398, (chr_index_end-chr_index_start)+1))
+    snp_id <- read.gdsn(index.gdsn(snps_gds, "snp.id"), start = chr_index_start, count=(chr_index_end-chr_index_start)+1)
+
+    genotypes_matrix = as.matrix(genotypes)
+    colnames(genotypes_matrix) = snp_id
+    rownames(genotypes_matrix) = sampleid
+
+    snp_list_chrom = snp_list[chromosome == paste(chrom)]
+    snp_list_chrom = snp_list_chrom[order(hg19_pos)]
+
+    genotypes_snp_list = matrix(nrow = 398, ncol = length(unique(snp_list_chrom$snp)))
+    colnames(genotypes_snp_list) = unique(snp_list_chrom$snp)
+    count = 1
+    for (snp in unique(snp_list_chrom$snp)){
+        index = match(snp,snp_id)
+        genotypes_snp_list[,count] = genotypes_matrix[,index]
+        count = count + 1
+    }
+    genotypes_snp_list[genotypes_snp_list==3]<-NA
+    
+    # get rid of columns containing ALL NA
+    genotypes_snp_list_no_NA = genotypes_snp_list[, colMeans(is.na(genotypes_snp_list)) != 1]
+    LD_matrix = ld.Rsquare(genotypes_snp_list_no_NA)
+
+    # Now find LD blocks
+    correlated_snps = data.table()
+    start = 1
+    cluster_id = 0
+    for (end in 1:ncol(LD_matrix)){
+        correlated = all(LD_matrix[start:end, start:end] > cutoff | LD_matrix[start:end, start:end] < -1*cutoff)
+        if (start != end & end == ncol(LD_matrix)){
+                cluster_id = cluster_id + 1
+                correlated_snps = rbind(correlated_snps, data.table(snp = c(colnames(LD_matrix[start:end, start:end])), cluster = rep(cluster_id, length(colnames(LD_matrix[start:end, start:end])))))
+                print(paste0('block', start, '_', (end)))
+                next
+            }
+        if (correlated == TRUE){
+            next
+        } else {
+            if (start != end-1){
+                cluster_id = cluster_id + 1
+                correlated_snps = rbind(correlated_snps, data.table(snp = c(colnames(LD_matrix[start:end-1, start:end-1])), cluster = rep(cluster_id, length(colnames(LD_matrix[start:end-1, start:end-1])))))
+                print(paste0('block', start, '_', (end-1)))
+            }
+            start = end
+        }
     }
     closefn.gds(snps_gds)
-}
-
-group_p_values <- function(snp_id_list, snp_meta_data, productivity, trim_type, bonferroni, proximity_cutoff, pvalue_cutoff){
-    correlated_snps = correlate_snps(snp_id_list, snp_meta_data, productivity, trim_type, bonferroni, proximity_cutoff, pvalue_cutoff)
-    if (nrow(correlated_snps) >= 1){
-        correlated_snps_with_group_p_val = data.table()
-        snps_gds = snpgdsOpen("../_ignore/snp_data/HSCT_comb_geno_combined_v03_tcr.gds")
-        n <- index.gdsn(snps_gds, "sample.id")
-        sampleid <- read.gdsn(n)
-        for (i in unique(correlated_snps$index)){
-            if(length(correlated_snps$feature) > 0){
-                snps = unique(correlated_snps[index == i][,-c(8,10)])
-            } else {
-                snps = unique(correlated_snps[index == i])
-            }
-            if (nrow(snps)>1){
-                genotype_list = data.table(scanID = c(sampleid))
-                colnames(genotype_list) = c("scanID")
-                genotype_list$scanID = as.numeric(as.character(genotype_list$scanID))
-                subject_id_mapping = as.data.table(read.table('../_ignore/snp_data/gwas_id_mapping.tsv', sep = "\t", fill=TRUE, header = TRUE, check.names = FALSE))
-                genotypes_subjects = merge(genotype_list, subject_id_mapping, by = "scanID")
-                for (snp in snps$snp){
-                    snp_id = as.numeric(substring(snp, 4))
-                    genotype = snpgdsGetGeno(snps_gds, snp.id=snp_id)
-                    genotypes_df = data.table(genotype)
-                    colnames(genotypes_df) = c(snp)
-                    # Convert subject names and compile condensed data: 
-                    genotypes_subjects = cbind(genotypes_subjects, genotypes_df)
-                }
-                #This may throw an error if there are NA entries: 
-                snps$group_p_val = COMBAT(x = snps$p, snp.ref = genotypes_subjects[,-c(1,2)])[1]
-            } else {
-                snps$group_p_val = snps$p
-            }
-            correlated_snps_with_group_p_val= rbind(correlated_snps_with_group_p_val, snps)
-        }
-        closefn.gds(snps_gds)
-        print("finished p-value grouping")
-
-        name = paste0('regression_bootstrap_results/', productivity, '/', trim_type, '/', trim_type, '_correlated_snps_from_snplist_', snp_id_list[1], "-",snp_id_list[length   (snp_id_list)], '_snps_varying_intercepts_by_subject.tsv')
-
-        write.table(correlated_snps_with_group_p_val, file= name, quote=FALSE, sep='\t', col.names = NA)
-    } else {
-        print("no correlated snps!!!")
-    } 
+    return(correlated_snps)
 }
--- trimming_regression/manha_visualization.R
+++ trimming_regression/manha_visualization.R
@@ -7,7 +7,7 @@ library(RColorBrewer)
 source('trimming_regression_functions.R')
 
 
-compile_data_manhattan <- function(snp_meta_data, snp_id_list, productivity, varying_int, trim_type, chromosome, correlate_snps, gene_conditioning, weighting){
+compile_data_manhattan <- function(snp_meta_data, snp_id_list, correlated_snps, productivity, varying_int, trim_type, chromosome, correlate_snps, gene_conditioning, weighting){
     bonferroni = 0.05/35481497
 
     # sort the snp meta data from phil (i.e. assign common column names)
@@ -20,26 +20,14 @@ compile_data_manhattan <- function(snp_meta_data, snp_id_list, productivity, var
         colnames(snp_meta_data) = c("chr", "feature", "hg19_pos", "phil_pval", "snp")
     } else {
         snp_meta_data = snp_meta_data[,1:3]
-        snp_meta_data$snpid = paste0("snp", snp_meta_data$snpid)
-        colnames(snp_meta_data) = c("snp", "hg19_pos", "chr")
+        snp_meta_data$snp = paste0("snp", snp_meta_data$snp)
+        colnames(snp_meta_data) = c("snp", "chr", "hg19_pos")
     }
 
     if (productivity == 'productive'){
         productivity_status = "True"
     } else if (productivity == 'NOT_productive'){
         productivity_status = "False"
-    }
-
-    # load regression / pvalue results
-    regression_snp_list_all = data.table()
-    for (gene in c('True', 'False')){
-        for (weight in c('True', 'False')){
-            assign("data_temp", as.data.table(read.table(generate_file_name(snp_id_list, trim_type, productivity = productivity_status, gene_conditioning = gene, weighting = weight), header = TRUE, col.names = c('snp', 'intercept', 'slope', 'standard_error', 'pvalue')))[,c(1,5)])
-            gene_cond = ifelse(gene == 'True', '_gene_conditioning', '')
-            weight_cond = ifelse(weight == 'True', '_weight', '')
-            data_temp$regression_type = paste0('lmer', gene_cond, weight_cond)
-            regression_snp_list_all = rbind(regression_snp_list_all, data_temp)
-        }
     }
 
     # load regression / pvalue results
@@ -54,22 +42,13 @@ compile_data_manhattan <- function(snp_meta_data, snp_id_list, productivity, var
     }
 
     if (correlate_snps == 'True'){
-        # load correlated snps list (if it exists)
-        correlated_snps_filename = paste0('regression_bootstrap_results/', productivity, '/', trim_type, '/', trim_type, '_correlated_snps_from_snplist_', snp_id_list[1], "-",snp_id_list[length(snp_id_list)], '_snps_varying_intercepts_by_subject_with_gene.tsv')
-        if (file.exists(correlated_snps_filename)){
-            regression_snp_list_correlated = as.data.table(read.table(correlated_snps_filename, header = TRUE))[, -c(7:9)]
-            colnames(regression_snp_list_correlated) = c("index", colnames(regression_snp_list))
-            regression_snp_list_no_correlated = subset(regression_snp_list, !(snp %in% regression_snp_list_correlated$snp))
-            correlated_snps = paste0("Correlated snps shown by color")
-            # define dataframe containing correlated snps and meta data
-            together_correlated = merge(regression_snp_list_correlated, snp_meta_data, by = "snp", all.x = TRUE)
-        } else {
-            regression_snp_list_correlated = data.table()
-            regression_snp_list_no_correlated = regression_snp_list
-            correlated_snps = paste0("No correlated snps present!")
-            # define dataframe containing correlated snps and meta data
-            together_correlated = data.table()
-        }
+        regression_snp_list_correlated = correlated_snps
+        regression_snp_list_correlated$snp = paste0('snp', regression_snp_list_correlated$snp)
+        regression_snp_list_correlated = merge(regression_snp_list_correlated, regression_snp_list, all.x = TRUE)
+        regression_snp_list_no_correlated = subset(regression_snp_list, !(snp %in% regression_snp_list_correlated$snp))
+        correlated_snps = paste0("Correlated snps shown by color")
+        # define dataframe containing correlated snps and meta data
+        together_correlated = merge(regression_snp_list_correlated, snp_meta_data, by = "snp", all.x = TRUE)
     } else {
         regression_snp_list_correlated = data.table()
         regression_snp_list_no_correlated = regression_snp_list
@@ -108,7 +87,7 @@ compile_data_manhattan <- function(snp_meta_data, snp_id_list, productivity, var
     together_not_correlated = together_not_correlated[chr == chromosome]
     together_other = together_other[chr == chromosome]
 
-    par(mar=c(5,6,4,4)+.1)
+    par(mar=c(5,5,7,5)+.1)
     if (nrow(together_not_correlated) > 0){
         if (nrow(together_correlated) > 0){
             xlimit = c(min(together_not_correlated$hg19_pos, together_correlated$hg19_pos)-1000, max(together_not_correlated$hg19_pos, together_correlated$hg19_pos)+1000)
@@ -119,10 +98,10 @@ compile_data_manhattan <- function(snp_meta_data, snp_id_list, productivity, var
         }
         plot(as.numeric(together_not_correlated$hg19_pos), as.numeric(-1*log(together_not_correlated$pvalue, base =10)), bg = alpha("black", 0.4), col = alpha("black", 0.9), xlab = paste0('Chromosome ', chromosome,' position'), ylab = '-log10(p value)', main = paste0('P-value of SNP effect on ', trim_type, ' for ', productivity, " TCRs on ch", chromosome, "\n", correlated_snps, "\n", regression_type), panel.first = grid(), cex.main=1.5, cex.lab=1.5, cex.axis=1, pch = 21, cex = 1.5, xlim = xlimit, ylim = ylimit)
 
-        palette(brewer.pal(n = length(unique(together_correlated$index)), name = 'Set3'))
-        col = setNames(palette(), levels(together_correlated$index))
+        palette(brewer.pal(n = length(unique(together_correlated$cluster)), name = 'Set2'))
+        col = setNames(palette(), levels(as.factor(together_correlated$cluster)))
         if (nrow(together_correlated) > 0){
-            points(together_correlated$hg19_pos, -1*log(together_correlated$pvalue, base =10), bg = c(factor(together_correlated$index), alpha = 0.4), col = alpha("black", 0.9), pch=21, cex = 1.5)
+            points(together_correlated$hg19_pos, -1*log(together_correlated$pvalue, base =10), bg = alpha(col[together_correlated$cluster], 0.8), col = alpha("black", 0.9), pch=21, cex = 1.5)
         }
         points(as.numeric(together_other$hg19_pos), -1*log(together_other$pvalue, base =10), col = alpha("red", 0.9), pch=1, cex = 1.5)
     } else {
@@ -130,16 +109,16 @@ compile_data_manhattan <- function(snp_meta_data, snp_id_list, productivity, var
             xlimit = c(min(together_correlated$hg19_pos)-1000, max(together_correlated$hg19_pos)+1000)
             ylimit = c(0, max(-1*log(together_correlated$pvalue, base =10))+5)
         }
-        palette(brewer.pal(n = length(unique(together_correlated$index)), name = 'Set3'))
-        col = setNames(palette(), levels(together_correlated$index))
+        palette(brewer.pal(n = length(unique(together_correlated$cluster)), name = 'Set2'))
+        col = setNames(palette(), levels(as.factor(together_correlated$cluster)))
 
-        plot(together_correlated$hg19_pos, -1*log(together_correlated$pvalue, base =10), bg = c(factor(together_correlated$index), alpha = 0.4), col = alpha("black", 0.9), xlab = paste0('Chromosome ', chromosome,' position'), ylab = '-log10(p value)', main = paste0('P-value of SNP effect on ', trim_type, ' for ', productivity, " TCRs on ch", chromosome, "\n", correlated_snps, "\n", regression_type), panel.first = grid(), cex.main=1.5, cex.lab=1.5, cex.axis=1, pch = 21, cex = 1.5, xlim = c(min(together_not_correlated$hg19_pos, together_correlated$hg19_pos)-1000, max(together_not_correlated$hg19_pos, together_correlated$hg19_pos)+1000), ylim = c(0, max(-1*log(together_not_correlated$pvalue, base =10), -1*log(together_correlated$pvalue, base =10))+5))
+        plot(together_correlated$hg19_pos, -1*log(together_correlated$pvalue, base =10), bg = alpha(col[together_correlated$cluster], 0.8), col = alpha("black", 0.9), xlab = paste0('Chromosome ', chromosome,' position'), ylab = '-log10(p value)', main = paste0('P-value of SNP effect on ', trim_type, ' for ', productivity, " TCRs on ch", chromosome, "\n", correlated_snps, "\n", regression_type), panel.first = grid(), cex.main=1.5, cex.lab=1.5, cex.axis=1, pch = 21, cex = 1.5, xlim = c(min(together_not_correlated$hg19_pos, together_correlated$hg19_pos)-1000, max(together_not_correlated$hg19_pos, together_correlated$hg19_pos)+1000), ylim = c(0, max(-1*log(together_not_correlated$pvalue, base =10), -1*log(together_correlated$pvalue, base =10))+5))
 
         points(as.numeric(together_other$hg19_pos), -1*log(together_other$pvalue, base =10), col = alpha("red", 0.9), pch=1, cex = 1.5)
     }
 
     abline(h = -1*log(bonferroni, base = 10), col = "red", lwd = 4, lty = 2)
-    legend("topleft", box.lty=0, legend=c("-log10(bonferroni)", paste0("significant for ", prod, " TCRs"), "not in a cluster"), col=c("red", alpha("red", 0.9), alpha("black", 0.4)), lty=c(2, NA, NA), lwd = c(3, NA, NA), pch = c(NA, 1, 19), cex = 1.5)
+    legend("topleft", box.lty=0, legend=c("-log10(bonferroni)", paste0("significant for ", prod, " TCRs"), "not in a cluster", rep("cluster", length(unique(together_correlated$cluster)))), col=c("red", alpha("red", 0.9), alpha("black", 0.4), alpha(col, 0.8)), lty=c(2, NA, NA, rep(NA, length(unique(together_correlated$cluster)))), lwd = c(3, NA, NA,rep(NA, length(unique(together_correlated$cluster)))), pch = c(NA, 1, 19,rep(19, length(unique(together_correlated$cluster)))), cex = 1.5)
 }
 
 
@@ -197,6 +176,11 @@ compare_phil_ALL <- function(phil_p_vals, snp_id_list, productivity, varying_int
         }
     }
 
+    simple_regression_snp_list = as.data.table(read.table(paste0("regression_bootstrap_results/", productivity, "/", trim_type, "/", trim_type, "_", productivity, "_snplist_", snp_id_list[1], "-",snp_id_list[length(snp_id_list)], "_snps_simple.tsv"), header = TRUE))[,c(1,5)]
+    simple_regression_snp_list$regression_type = 'glm'
+
+    regression_snp_list_all = rbind(regression_snp_list_all, simple_regression_snp_list)
+
     phil_p_vals$snpid = paste0("snp", phil_p_vals$snpid)
     trimming_feature = paste0(trim_type, "_", productivity_frame)
     phil_p_vals_filtered = phil_p_vals[feature == trimming_feature][,-c(1:3)]
@@ -209,9 +193,9 @@ compare_phil_ALL <- function(phil_p_vals, snp_id_list, productivity, varying_int
     together[is.na(pvalue)]$pvalue<-1
 
     palette(brewer.pal(n = length(unique(together$regression_type)), name = 'Set2'))
-    col = setNames(palette(), levels(together$regression_type))
+    col = setNames(palette(), levels(as.factor(together$regression_type)))
 
-    plot(jitter(-1*log(together$linreg_pval, base =10), 0.5), -1*log(together$pvalue, base =10), xlim = c(0, 70), ylim = c(0, 70), col = alpha(col, 0.6), pch=19, cex = 1.5, ylab = paste0('P values from lmer'), xlab = 'P values from Phils Analysis', main = paste0('P value comparison for ', trim_type, ' for ', productivity, " TCRs"), cex.main=1.5, cex.lab=1.5, cex.axis=1, panel.first = grid())
+    plot(jitter(-1*log(together$linreg_pval, base =10), 0.5), -1*log(together$pvalue, base =10), xlim = c(0, 90), ylim = c(0, 90), col = alpha(col[together$regression_type], 0.6), pch=19, cex = 1.5, ylab = paste0('P values from lmer'), xlab = 'P values from Phils Analysis', main = paste0('P value comparison for ', trim_type, ' for ', productivity, " TCRs"), cex.main=1.5, cex.lab=1.5, cex.axis=1, panel.first = grid())
     abline(a = 0, b = 1, lty = 2, lwd = 4)
-    legend("bottomright", box.lty=0, legend=unique(together$regression_type), col= alpha(col, 0.6), pch = c(rep(19, 4)), cex = 1.5)
+    legend("bottomright", box.lty=0, legend=levels(as.factor(together$regression_type)), col= alpha(col, 0.6), pch = c(rep(19, 4)), cex = 1.5)
 }
