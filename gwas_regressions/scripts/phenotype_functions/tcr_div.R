f6bd76c11a4b38d4fe7b483e83abffb6754ad129
magdalenarussell <magdalenarussell23@gmail.com>
2021-01-27 17:04:24 -0800

    some gitignore changes

--- gwas_regressions/scripts/phenotype_functions/tcr_div.R
+++
@@ -1,4 +0,0 @@
-GENE_TYPE <<- 'NA' 
-PHENOTYPE_CLASS <<- 'tcr_div'
-
-source(paste0(PROJECT_PATH, '/tcr-gwas/gwas_regressions/scripts/phenotype_functions/phenotype_classes/', PHENOTYPE_CLASS, '_class_functions.R'))
