f2606b4436e834aae92046d8874c3ad85310bdae
magdalenarussell <magdalenarussell23@gmail.com>
2021-05-13 10:40:11 -0700

    Remove the now ignored files

--- gwas_regressions/plotting_scripts/plotting_functions/regression_parameters.R
+++
@@ -1,5 +0,0 @@
-set_regression_parameters <- function(phenotype){
-    source(paste0(PROJECT_PATH, '/tcr-gwas/gwas_regressions/scripts/phenotype_functions/', phenotype, '.R'))
-    parameters = c(phenotype = phenotype, condensing_variable = CONDENSING_VARIABLE, keep_missing_d_gene = KEEP_MISSING_D_GENE, pca_count = as.character(PCA_COUNT))
-    return(parameters)
-}
